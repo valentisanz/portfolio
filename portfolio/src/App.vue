954d75f12b1946c22098090c6076da83c98ba930
valenti <valentisanz99@gmail.com>
2020-11-23 19:20:39 +0100

    first tile structure

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,17 +1,60 @@
 <template>
   <div id="app">
-    <button class="button is-primary">This is a button</button>
-    
+    <div class="tile is-ancestor">
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child box">
+          <p class="title">JavaScript</p>
+          <progress class="progress is-success" value="60" max="100">
+            60%
+          </progress>
+        </div>
+        <div class="tile is-child box">
+          <p class="title">JavaScript</p>
+          <progress class="progress is-success" value="60" max="100">
+            60%
+          </progress>
+        </div>
+      </div>
+      <Profile />
+
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child box">
+          <p class="title">One</p>
+        </div>
+        <div class="tile is-child box">
+          <p class="title">Two</p>
+          <p class="subtitle">el pepe</p>
+        </div>
+      </div>
+    </div>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent is-12">
+        <article class="tile is-child box">
+          <p class="title">Main column</p>
+          <p class="subtitle">With some content</p>
+          <div class="content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+              non massa sem. Etiam finibus odio quis feugiat facilisis.
+            </p>
+          </div>
+        </article>
+      </div>
+    </div>
+    <footer class="footer">
+      <p class="has-text-centered"><strong>Valenti Sanz</strong> 2020</p>
+    </footer>
   </div>
 </template>
 
 <script>
+import Profile from "./components/Profile";
 export default {
   name: "App",
-  components: {},
+  components: { Profile },
 };
 </script>
 
 <style lang='scss'>
-@import "~bulma/css/bulma.css";
 </style>
---
+++ portfolio/src/components/Profile.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tile is-parent">
+    <div class="tile is-child box">
+      <p class="title">{{ state.name }}</p>
+      <p class="usbtitle">{{ state.bio }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "Profile",
+  setup() {
+    const state = reactive({
+      name: "Valenti Sanz",
+      bio: "Full Stack Developer",
+    });
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
