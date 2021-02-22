be1330a9202bd104fd08cc8692067f7836c02417
valenti <valentisanz99@gmail.com>
2021-02-22 19:10:31 +0100

    Sidebar and buttons commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import './index.scss'
+import Home from './pages/Home/Home'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route path='/home' exact component={Home} />
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
 export default App;
