5036149110519bbe584990ccf79d66b80e0abe95
valenti <valentisanz99@gmail.com>
2021-03-23 19:14:26 +0100

    New global styles and transitions added

--- portfolio/src/pages/Home/Home.js
+++ portfolio/src/pages/Home/Home.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './Home.scss'
 import Image from '../../assets/photo.png'
+import { Fade, Flip } from "react-awesome-reveal";
+
 function Home() {
     return (
         <div className='home-page'>
-            <div className='image-container'>
-                <img src={Image} />
-            </div>
             <div className='info-container'>
-                <h2>Name:</h2>
-                <h1>Valenti Sanz</h1>
-                <h2>Description:</h2>
-                <h1>Full Stack Developer</h1>
-                <h2>Age:</h2>
-                <h1>21 years old</h1>
+                <Flip triggerOnce>
+                    <img src={Image} />
+                </Flip>
+                <Fade triggerOnce delay={1000} >
+                    <h3>Welcome! My name is</h3>
+                    <h1>Valenti Sanz</h1>
+                    <h3>I'm a Full Stack Developer</h3>
+                </Fade>
+
             </div>
         </div>
     )
